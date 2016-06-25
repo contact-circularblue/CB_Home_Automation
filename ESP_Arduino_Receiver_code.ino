9308bac198fc4487f6f14d6aece408fe967413a0
contact-circularblue <contact@circularblue.com>
2016-06-25 13:43:28 +0530

    Update ESP_Arduino_Receiver_code.ino

--- ESP_Arduino_Receiver_code.ino
+++ ESP_Arduino_Receiver_code.ino
@@ -7,8 +7,6 @@
   int Fan_Speed; // Stores the current fan speed
 */
 
-///Added GET for all feilds and make a string containing all these values which is to be sent to the main IC
-
 
 
 #include <SoftwareSerial.h>
