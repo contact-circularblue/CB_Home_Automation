279c707bf8acb8e816b110f3c18d6f3c975b04ae
contact-circularblue <contact@circularblue.com>
2016-06-25 17:41:24 +0530

    Add files via upload
    GET is completely working.

--- ESP_Arduino_Receiver_code.ino
+++ ESP_Arduino_Receiver_code.ino
@@ -1,3 +1,4 @@
+
 /*
   int pushButton_L1=2;     //light_1   // Digital pin 2 of arduino
   int pushButton_L2=3;     //Light_2   // Digital pin 3 of arduino
@@ -7,11 +8,21 @@
   int Fan_Speed; // Stores the current fan speed
 */
 
+
+#include <EEPROM.h>
 #include <SoftwareSerial.h>
 
 String data = "";
+
+String send_to_main = "";
+
 void ESP_post();
 void ESP_get();
+bool Connect_To_Thingspeak();
+bool start_transmission(int data_length);
+String send_data(String data, char field);
+bool Wifi_Connect();
+ISR(TIMER1_OVF_vect);
 
 SoftwareSerial ESP(10, 11); //RX,TX of Arduino
 const int Power_LED =  12;
@@ -35,6 +46,26 @@ void setup()
 void loop()
 {
 
+  //start:
+  bool initialise=ESP_initialise();
+
+  if(initialise==1)
+  {
+
+  // connecting to mobile hotspot
+  if(EEPROM.read(101)==0)
+  {
+  bool HotspotConnect=Hotspot_Connect();
+  }
+  
+
+  // Connecting to home wifi
+  if(EEPROM.read(101)==1)
+  {
+  bool WifiConnect=Wifi_Connect();
+  }
+  while(1)
+  {
   if (Serial.available())
   {
     ESP_post();
@@ -43,9 +74,11 @@ void loop()
   {
     ESP_get();
   }
+  }
+  }
+  
 }
-
-
+/////////////////////////////////////////////////////////////////////////////////
 void ESP_post()
 {
   String stat = "";                     // create a string to store the data available on serial port
@@ -65,8 +98,37 @@ void ESP_post()
 
   Serial.println(stat);
 
+  bool fwd;
+  bool transmisson;
   if (stat.substring(0) == "L1=0")  // Light_1
   {
+
+    fwd = 0;
+    while (fwd == 0)
+    {
+      bool Connect = Connect_To_Thingspeak();
+      if (Connect == 1)
+        fwd = 1;
+        else
+      Wifi_Connect();
+    }
+    send_to_main = "";
+    data = "";
+    data = F("https://api.thingspeak.com/update?api_key=VZGWTTS9IC63O7AG;&field1=0");  // light_1
+
+    transmisson = start_transmission(data.length());
+    if (transmisson != 1)
+    {
+      bool connection;
+      while (connection != 1)
+      {
+        connection = Wifi_Connect();
+      }
+    }
+
+    String L1 = send_data(data, '1');
+
+
     data = F("https://api.thingspeak.com/update?api_key=VZGWTTS9IC63O7AG;&field1=0");  // using F() so that the string initially is stored only in ROM and not copied in RAM
     Serial.println("L1 is OFF");
     Serial.println(data);
@@ -155,14 +217,288 @@ void ESP_post()
   }
 }
 
-
+///////////////////////////////////////////////////////////////////////
 void ESP_get()
 {
-  //start:
+  // Finally fetch data for fields from the server
+
+L1_again:
+
+  //////////////////////   Light_1   ///////////////////////////////
+
+  bool fwd = 0;
+  bool transmisson = 0;
+  while (fwd == 0)
+  {
+    bool Connect = Connect_To_Thingspeak();
+    if (Connect == 1)
+      fwd = 1;
+      else
+      Wifi_Connect();
+  }
+  send_to_main = "";
+  data = "";
+  data = "GET /channels/103571/fields/1/last.json?";   // light_1
+
+  transmisson = start_transmission(data.length());
+  if (transmisson != 1)
+  {
+    bool connection;
+    while (connection != 1)
+    {
+      connection = Wifi_Connect();
+    }
+  }
+
+  String L1 = send_data(data, '1');
+
+  if (L1 == "0" || L1 == "1")
+  {
+    send_to_main += "L1=";
+    send_to_main += L1;
+  }
+  else
+  {
+    goto L1_again;
+  }
+  Serial.print("Light 1=");
+  Serial.println(L1);
+
+  if (L1 == "1")
+  {
+
+    digitalWrite(4, HIGH);
+  }
+
+  if (L1 == "0")
+  {
+    digitalWrite(4, LOW);
+  }
+
+  //////////// LIGHT 2 ////////////
+
+  fwd = 0;
+  while (fwd == 0)
+  {
+    bool Connect = Connect_To_Thingspeak();
+    if (Connect == 1)
+      fwd = 1;
+      else
+Wifi_Connect();
+}
+
+  data = "";
+  data = "GET /channels/103571/fields/2/last.json?";   // light_1
+
+  transmisson = start_transmission(data.length());
+  if (transmisson != 1)
+  {
+    bool connection;
+    while (connection != 1)
+    {
+      connection = Wifi_Connect();
+    }
+  }
+
+  String L2 = send_data(data, '2');
+
+  send_to_main += "&L2=";
+  send_to_main += L2;
+
+  Serial.print("Light 2=");
+  Serial.println(L2);
+
+
+  //////////// Fan ////////////
+
+  fwd = 0;
+  while (fwd == 0)
+  {
+    bool Connect = Connect_To_Thingspeak();
+    if (Connect == 1)
+      fwd = 1;
+      else
+Wifi_Connect();
+}
+
+  data = "";
+  data = "GET /channels/103571/fields/3/last.json?";   // light_1
+
+  transmisson = start_transmission(data.length());
+  if (transmisson != 1)
+  {
+    bool connection;
+    while (connection != 1)
+    {
+      connection = Wifi_Connect();
+    }
+  }
+  String F1 = send_data(data, '3');
+
+  send_to_main += "&F1=";
+  send_to_main += F1;
+
+  Serial.print("Fan=");
+  Serial.println(F1);
+
+  //////////// Fan Speed////////////
+
+  fwd = 0;
+  while (fwd == 0)
+  {
+    bool Connect = Connect_To_Thingspeak();
+    if (Connect == 1)
+      fwd = 1;
+      else
+Wifi_Connect();
+}
+
+  data = "";
+  data = "GET /channels/103571/fields/4/last.json?"; // fan_ speed
+
+  transmisson = start_transmission(data.length());
+  if (transmisson != 1)
+  {
+    bool connection;
+    while (connection != 1)
+    {
+      connection = Wifi_Connect();
+    }
+  }
+
+  String fan_speed = send_data(data, '4');
+
+  send_to_main += "&Fs=";
+  send_to_main += fan_speed;
+
+  Serial.print("Speed=");
+  Serial.println(fan_speed);
+
+  if (fan_speed == "1")
+  {
+    digitalWrite(5, HIGH);
+    digitalWrite(6, LOW);
+    digitalWrite(7, HIGH);
+    digitalWrite(8, LOW);
+  }
+
+  if (fan_speed == "2")
+  {
+    digitalWrite(5, HIGH);
+    digitalWrite(6, LOW);
+    digitalWrite(7, LOW);
+    digitalWrite(8, HIGH);
+  }
+
+
+  Serial.print(send_to_main);
+
+  while (ESP.available())
+    ESP.read();
+
+
+}
+
+
+
+//////////////////////    ///////////////////////   ///////////////////////   ////////////////////////
+bool Connect_To_Thingspeak()
+{
+Connect_again:
+
+  ESP.println("AT+CIPSTART=4,\"TCP\",\"184.106.153.149\",80");
+  delay(3000);
+
+  if (ESP.find("CONNECT"))
+  {
+    Serial.println("Linked to Thingspeak.com");
+    while (ESP.available())
+      ESP.read();
+    return 1;
+  }
+  else
+  {
+    Serial.println("Error connecting to Thingspeak.com");
+    while (ESP.available())
+      ESP.read();
+   // goto Connect_again;
+    return 0;
+    //goto CIPMUX;
+  }
+}
+
+bool start_transmission(int data_length)
+{
+  data_length = data_length + 2;
+  String data = "";
+  data += "AT+CIPSEND=4,";
+  data += data_length;
+
+  ESP.println(data);
+  delay(1000);
+  if (ESP.find(">"))
+  {
+    Serial.println("> received");
+    while (ESP.available())
+      ESP.read();
+    return 1;
+  }
+  else
+  {
+    Serial.println("Error in command AT+CIPSEND");
+    while (ESP.available())
+      ESP.read();
+    return 0;
+  }
+
+}
+
+String send_data(String data, char field)
+{
+  String field_check = "";
+  field_check += "\"field";
+  field_check += field;
+  field_check += "\":\"";
+
+  char buff[field_check.length() + 1];
+  field_check.toCharArray(buff, field_check.length() + 1);
+
+  ESP.println(data);
+
+  /*
+    while(1)
+    while(ESP.available())
+    Serial.print(char(ESP.read()));
+  */
+
+  String field_read = "";
+
+  if (ESP.find(buff))
+  {
+    //Serial.println("+IPD received");
+    delay(200);
+    while (ESP.available())
+    {
+      char c = ESP.read();
+      if (c == '\"')
+        break;
+      field_read += c;
+      delay(100);
+    }
+  }
+  //Serial.println("11111");
+  Serial.flush();
+  return field_read;
+}
+
+/////////////////////////////////////////////////////////////////
+
+bool ESP_initialise()
+{
   while (ESP.available())
     ESP.read();
   delay(1000);
-  
+
   ESP.println (F("AT+CIOBAUD=9600"));
   delay(1000);
   if (ESP.find("OK"))
@@ -225,15 +561,23 @@ CIPMUX:
   ESP.println(F("AT+CWMODE=1"));
   delay(500);
   if (ESP.find("OK"))
+  {
     Serial.println(F("MODE is set to 1"));
   while (ESP.available())
     ESP.read();
+    return 1;
+  }
+  else
+  return 0;
+
+}
+
+//////////////////////////////////////////////////////////////////
 
 
+bool Hotspot_Connect()
+{
 
-  //while(1);
-
-  ////////////////////////
 Wifi_Connect:
   noInterrupts();           // disable all interrupts
   TCCR1A = 0;
@@ -245,7 +589,7 @@ Wifi_Connect:
   interrupts();
 
 
-  Serial.println(F("Connecting to WiFi"));
+  Serial.println(F("Connecting to Hotspot"));
   ESP.println(F("AT+CWJAP=\"Circular Blue\",\"Vibgyor1!\""));
   delay(10000);
 
@@ -262,7 +606,7 @@ Wifi_Connect:
   if (a == 1)
   {
 
-    Serial.println(F("Wifi connection Successful"));
+    Serial.println(F("Hotspot connection Successful"));
 
     while (ESP.available())
       ESP.read();
@@ -273,16 +617,186 @@ Wifi_Connect:
 
   else
   {
-    Serial.println(F("Error connecting to wifi"));
+    Serial.println(F("Error connecting to Hotspot"));
 
     while (ESP.available())
       ESP.read();
     goto Wifi_Connect;
   }
+
+  bool Connect = Connect_To_Thingspeak();
+  if (Connect != 1)
+  {
+    ESP_initialise();
+    goto Wifi_Connect;
+  }
   
+
+  String data = "GET /channels/103571/fields/7/last.json?";
+
+  bool transmisson = start_transmission(data.length());
+  if (transmisson != 1)
+    goto Wifi_Connect;
+
+
+  String integer = send_data(data, '7');
+  Serial.print("integer=");
+  Serial.println(integer);
+  if (integer == "1")
+  {
+    int fwd = 0;
+    while (fwd == 0)
+    {
+      bool Connect = Connect_To_Thingspeak();
+      if (Connect == 1)
+        fwd = 1;
+    }
+    data = "";
+    data = "GET /channels/103571/fields/5/last.json?";
+
+    transmisson = start_transmission(data.length());
+    if (transmisson != 1)
+      goto Wifi_Connect;
+
+    String ssid = send_data(data, '5');
+    Serial.print("ssid=");
+    Serial.println(ssid);
+
+    int ssid_l = ssid.length();          // for storing ssid in eeprom
+    for (int i = 20; i < (20+ssid_l); i++)
+    {
+      EEPROM.write(i, ssid.charAt(i - 20));
+    }
+    EEPROM.write(50, ssid_l);            // forstoing length of ssid in eeprom
+
+    String check_ssid = "";
+    for (int i = 20; i < (20+EEPROM.read(50)); i++)
+    {
+      check_ssid += char(EEPROM.read(i));
+    }
+
+    Serial.println(check_ssid);
+
+    fwd = 0;
+    while (fwd == 0)
+    {
+      bool Connect = Connect_To_Thingspeak();
+      if (Connect == 1)
+        fwd = 1;
+    }
+
+    data = "";
+    data = "GET /channels/103571/fields/6/last.json?";
+
+    transmisson = start_transmission(data.length());
+    if (transmisson != 1)
+      goto Wifi_Connect;
+
+    String password = send_data(data, '6');
+    Serial.print("password=");
+    Serial.println(password);
+
+    int password_l = password.length();          // for storing ssid in eeprom
+    for (int i = 51; i < (51+password_l); i++)
+    {
+      EEPROM.write(i, password.charAt(i - 51));
+    }
+    EEPROM.write(100, password_l);
+
+
+    EEPROM.write(101,1);
+    Serial.println(EEPROM.read(101));
+
+    while (ESP.available())
+      ESP.read();
+  }
 }
 
-//////////////////////    ///////////////////////   ///////////////////////   ////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+bool Wifi_Connect()
+
+{
+Wifi_Connect2:
+
+  Serial.println("Connecting to HOME Wifi");
+
+  noInterrupts();           // disable all interrupts
+  TCCR1A = 0;
+  TCCR1B = 0;
+
+  TCNT1 = 34286;            // preload timer 65536-16MHz/256/2Hz
+  TCCR1B |= (1 << CS12);    // 256 prescaler
+  TIMSK1 |= (1 << TOIE1);   // enable timer overflow interrupt
+  interrupts();
+
+  // Connect to home WiFi network
+
+  String check_ssid = "";
+    for (int i = 20; i < (20+EEPROM.read(50)); i++)
+    {
+      check_ssid += EEPROM.read(i);
+    }
+
+   String check_password = "";
+    for (int i = 51; i < (51+EEPROM.read(100)); i++)
+    {
+      check_password += EEPROM.read(i);
+    }
+
+  
+  String ssid="";
+  ssid=check_ssid;
+  String password=""; 
+  password=check_password;
+
+  Serial.println(ssid);
+  Serial.println(password);
+
+  //while(1);
+  String Wifi_details = "AT+CWJAP=\"";
+  Wifi_details += ssid;
+  Wifi_details += "\",\"";
+  Wifi_details += password;
+  Wifi_details += "\"";
+
+  ESP.println(Wifi_details);
+  delay(10000);
+  /*
+    if(ESP.find("GOT IP"))
+    Serial.println("CONNECTED TO HOME WIFI");
+    else
+    Serial.println("NOT CONNECTED");
+  */
+  while (ESP.available())
+    ESP.read();
+
+
+  ESP.println("AT+CWJAP?");
+  delay(5000);
+
+  bool a = ESP.find("Circular");
+
+
+  if (a == 1)
+  {
+
+    ////////// LIGHT 1 ////////////
+
+    Serial.println("HOME Wifi connection Successful");
+    while (ESP.available())
+      ESP.read();
+    TCCR1B = 0;
+    //noInterrupts();
+    digitalWrite(WiFi_LED, HIGH);
+    return 1;
+
+  }
+  else
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
 
 ISR(TIMER1_OVF_vect)
 {
